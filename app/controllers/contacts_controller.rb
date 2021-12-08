bb08f699a80494f33e77b24364011d2d09fd0f13
Christian BeauxYeux <gildweek@gmail.com>
2021-12-08 11:42:06 +0400

    Added create method for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,4 +10,19 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contact_path(@contact)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :address, :country, :phone)
+  end
 end
