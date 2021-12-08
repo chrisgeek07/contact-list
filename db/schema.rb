800256bccbdd7030d9b2ed6600ba7a6759841af4
Christian BeauxYeux <gildweek@gmail.com>
2021-12-08 10:58:53 +0400

    Created migration for Contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_064255) do
+ActiveRecord::Schema.define(version: 2021_12_08_065702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "country"
+    t.integer "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
